270a7cc911426bbab4d7f9803c199815ca9d3577
Claudia Ling <claudia.ling1128@gmail.com>
2018-03-02 03:21:36 +0000

    exercise

---
+++ Hello1_2.py
@@ -0,0 +1,7 @@
+# 1. Hello, you!
+name = input('What is your name? ')
+print('Hello', name)
+
+# 2. HELLO, YOU!
+print('Hello', name.upper())
+print('YOUR NAME HAS', len(name), 'LETTERS IN IT! AWESOME!')

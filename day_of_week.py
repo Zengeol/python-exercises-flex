b159237761a5a7d6a1012bb9e2f3e243b2eb3936
Claudia Ling <claudia.ling1128@gmail.com>
2018-03-02 14:45:39 +0000

    exercise

--- day_of_week.py
+++ day_of_week.py
@@ -1,3 +1,5 @@
 # 4. Day of the Week
 # Given the following code that prompts the user for a day number (the int function converts a numeric string to a number)
 day = int(input('Day (0-6)? '))
+week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+print(week[day])

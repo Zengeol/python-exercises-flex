86bf165c461197b1a536eb7e80ced98e5534d1bb
Claudia Ling <claudia.ling1128@gmail.com>
2018-03-02 15:01:13 +0000

    Excercises

---
+++ degree_conversion.py
@@ -0,0 +1,5 @@
+# 6. Celsius to Fahrenheit
+# Prompt the user for a number in degrees Celsius, and convert the value to degrees in Fahrenheit and display it to the user.
+Celsius = int(input("Enter a temperature in Celsius: "))
+Fahrenheit = 9.0/5.0 * Celsius + 32
+print ("Temperature:", Celsius, "C = ", Fahrenheit, "F")

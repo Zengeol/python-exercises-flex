c7a15aab01c017b3a6c802e9c78c55a8a087fe8b
Claudia Ling <claudia.ling1128@gmail.com>
2018-03-02 03:29:21 +0000

    exercise

--- madlib.py
+++ madlib.py
@@ -1,5 +1,6 @@
-count = 0
-while count < 10:
-  count += 1
-  print("The count is", count)
-print("Done")
+# 3. Madlib
+# Prompt the user for the missing words to a Madlib sentence using the input function.
+print('Please fill in the blanks below:\n', '____(name)____\'s favorite subject in school is ____(subject)____.')
+name = input('What is your name? ')
+subject = input('What is your subject?')
+print (name, '\'s favorite subject in school is', subject,'.')
